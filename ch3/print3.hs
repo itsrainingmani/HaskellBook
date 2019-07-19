a86fe6e21ed153e99a79d43c25b520776c5e3bee
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-07-19 01:01:41 -0400

    Add code for Chapters 2 and 3 of the Haskell Book

---
+++ ch3/print3.hs
@@ -0,0 +1,22 @@
+-- print3.hs
+module Print3 where
+
+myGreeting :: String
+-- The above line reads: "myGreeting has the type String"
+myGreeting = "hello" ++ " world!"
+
+hello :: [Char]
+-- String is a type synonym for [Char]
+hello = "hello"
+
+world :: [Char]
+world = "world!"
+
+-- myGreeting, hello and world are defined at the top level of the module
+-- so they can be available anywhere else
+
+main :: IO()
+main = do
+    putStrLn myGreeting
+    putStrLn secondGreeting
+    where secondGreeting = concat [hello, " ", world]
---
+++ code/test.hs
@@ -0,0 +1,7 @@
+-- test.hs
+
+module Test where
+
+  waxOn = x * y
+    where x = y * 20
+          y = 300
